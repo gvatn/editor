17bdee83cec3b56fd5267c951bdf995e55c7f032
gvatn <gudmund.vatn@gmail.com>
2020-06-03 12:57:53 +0200

    Cleanup useDoc

--- next/components/ConnectionCtx.ts
+++ next/components/ConnectionCtx.ts
@@ -1,18 +1,46 @@
 import { createContext, useState, useContext } from 'react';
 
+import ReconnectingWebSocket from 'reconnecting-websocket';
+
+import sharedb from 'sharedb/lib/client';
+
+
+class Connection {
+    connInner: null | any;
+
+    isConnecting: boolean
+
+    constructor() {
+        this.connInner = null;
+        this.isConnecting = false;
+    }
+
+    async conn() {
+        if (this.connInner !== null) {
+            return Promise.resolve(this.connInner);
+        }
+        const socket = new ReconnectingWebSocket('ws://localhost:8080');
+        this.connInner = new sharedb.Connection(socket);
+        return Promise.resolve(this.connInner);
+    }
+}
 
 const ConnectionCtx = createContext(null);
 
 export default ConnectionCtx;
 
 function useConn() {
-    const connContext = useContext(ConnectionCtx) as any;
+    const connContext = useContext(ConnectionCtx) as Connection;
     const [conn, setConn] = useState(connContext.connInner);
     let requireConn = () => {
         console.log("Conn is", conn)
         if (conn !== null) {
             return;
         }
+        if (connContext.isConnecting) {
+            return;
+        }
+        connContext.isConnecting = true;
         connContext.conn().then(conn => {
             console.log("Setting");
             setConn(conn);
@@ -23,30 +51,44 @@ function useConn() {
 
 function useDoc(key) {
     const [doc, setDoc] = useState(null);
+    // While possibly creating the initial document,
+    // we just hold onto the instance here, and only update
+    // the above doc state when document is ready
+    // Being contained in an object, changes should not
+    // trigger render
+    const [localDoc, setLocalDoc] = useState({
+        instance: null
+    });
     const [conn, requireConn] = useConn();
     if (doc !== null) {
         return doc;
     }
     requireConn();
-    if (conn === null) {
-        return doc;
-    }
     // Treat null key as "key not ready"
-    if (key === null) {
+    if (conn === null || key === null || localDoc.instance !== null) {
         return doc;
     }
-    const rawDoc = conn.get('docs', key);
-    console.log("Got raw doc", rawDoc);
-    if (rawDoc.type === null) {
-        rawDoc.create({ items: [] }, function () {
-            console.log("Creating");
-            setDoc(rawDoc);
-        });
-    } else {
-        console.log("Already created");
-        setDoc(rawDoc);
-    }
+    localDoc.instance = conn.get('docs', key);
+    console.log("Got instance", localDoc.instance);
+    setLocalDoc(localDoc); // Not sure if this is necessary
+    localDoc.instance.fetch((err) => {
+        if (err) {
+            console.error(err);
+            return doc;
+        }
+        console.log("Fetched", localDoc.instance);
+        if (localDoc.instance.type === null) {
+            debugger;
+            localDoc.instance.create({ items: [] }, function () {
+                console.log("Created doc");
+                setDoc(localDoc.instance);
+            });
+        } else {
+            console.log("Already created");
+            setDoc(localDoc.instance);
+        }
+    });
     return doc;
 }
 
-export { useConn, useDoc };
+export { useConn, useDoc, Connection };
--- next/pages/_app.tsx
+++ next/pages/_app.tsx
@@ -1,27 +1,6 @@
 // import App from 'next/app'
 
-import ConnectionCtx from '../components/ConnectionCtx';
-
-import ReconnectingWebSocket from 'reconnecting-websocket';
-
-import sharedb from 'sharedb/lib/client';
-
-class Connection {
-    connInner: null | any;
-
-    constructor() {
-        this.connInner = null;
-    }
-
-    async conn() {
-        if (this.connInner !== null) {
-            return Promise.resolve(this.connInner);
-        }
-        const socket = new ReconnectingWebSocket('ws://localhost:8080');
-        this.connInner = new sharedb.Connection(socket);
-        return Promise.resolve(this.connInner);
-    }
-}
+import ConnectionCtx, { Connection } from '../components/ConnectionCtx';
 
 const conn = new Connection();
 
