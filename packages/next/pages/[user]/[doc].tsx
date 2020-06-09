6eb8b7f4d054aab96d80a609556b2bf3b7cf24bf
gvatn <gudmund.vatn@gmail.com>
2020-06-09 11:45:41 +0200

    Monorepo

--- packages/next/pages/[user]/[doc].tsx
+++ packages/next/pages/[user]/[doc].tsx
@@ -1,23 +1,38 @@
 import Head from 'next/head'
 
 import { useRouter } from 'next/router';
-import { useContext, useState, useEffect, useMemo } from 'react';
+import { useContext, useState, useEffect, useMemo, useCallback } from 'react';
 import ConnectionCtx, { useConn, useDoc } from '../../components/ConnectionCtx';
 
 import { createEditor } from 'slate';
 import { Slate, Editable, withReact } from 'slate-react'
 
+const withCustom = (editor) => {
+  const { apply } = editor;
+  editor.apply = (op) => {
+    console.log("Op", op);
+    apply(op);
+  };
+  return editor;
+};
+
 function Doc({ doc }) {
   console.log("Got doc", doc);
-  const editor = useMemo(() => withReact(createEditor()), []);
+  const editor = useMemo(() => withCustom(withReact(createEditor())), []);
   const [value, setValue] = useState<any>([
     {
       type: 'paragraph',
       children: [{ text: 'A line of text in a paragraph.' }],
     }
   ]);
+
+  const onChange = useCallback((newValue) => {
+    console.log(newValue);
+    setValue(newValue);
+  }, []);
+
   return (<div className="slate-test">
-    <Slate editor={editor} value={value} onChange={(newValue) => setValue(newValue)}>
+    <Slate editor={editor} value={value} onChange={onChange}>
       <Editable spellCheck={false} />
     </Slate>
   </div>);
